7638c018b84d1dd2d1905776d8ce2292481b3635
franabarca <101573112+franabarca@users.noreply.github.com>
2023-07-28 16:42:40 -0400

    nuevo efecto hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { BrowserView, MobileView } from 'react-device-detect';
 import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <div className="relative z-0">
-            <BrowserView>
-              <StarsCanvas />
-            </BrowserView>
-            <Hero />
-          </div>
-        </div>
-  
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-  
-        <div className="relative z-0">
-          <Contact />
-            <StarsCanvas />
-
-        </div>
+    <div className="relative z-0 bg-primary">
+    
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      
+        <Navbar />
+        
+        <Hero />
+        
       </div>
+  
+
+    
+      <About/>
+
+      <Experience/>
+      <Tech/>
+      <Works/>
+
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+    </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
-
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,29 @@ module.exports = {
         "hero-pattern": "url('/src/assets/herobg.png')",
         "about-pattern":"url('/src/assets/aboutbg.jpg')",
       },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [
     require('tailwindcss-animated')
