ea9c4b99185d97f382728c22d90251e8d5f406ca
franabarca <101573112+franabarca@users.noreply.github.com>
2023-07-28 15:21:21 -0400

    prueba mobile-divice-detect

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
+import { BrowserView, MobileView } from 'react-device-detect';
 import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-    
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      
-        <Navbar />
-        <div className="relative z-0">
-        <StarsCanvas/> 
-          <Hero />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <div className="relative z-0">
+            <BrowserView>
+              <StarsCanvas />
+            </BrowserView>
+            <Hero />
+          </div>
         </div>
-        
-        
-      </div>
   
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+  
+        <div className="relative z-0">
+          <Contact />
+            <StarsCanvas />
 
-    
-      <About/>
-
-      <Experience/>
-      <Tech/>
-      <Works/>
-
-      <div className="relative z-0">
-        <Contact/>
-        <StarsCanvas/>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
