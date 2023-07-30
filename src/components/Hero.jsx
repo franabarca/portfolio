00ded590800320a9cd69f71f36b8ae83643b74d5
franabarca <101573112+franabarca@users.noreply.github.com>
2023-07-29 23:33:20 -0400

    cambio por un boton

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,20 +12,9 @@ const Hero = () => {
 
 
   return (
-    < >
-
+    <>
       <div className='container flex flex-col items-center justify-center gap-5 mx-auto px-4 sm:px-8 md:px-16 lg:px-32 mt-4'>
-        <div className='animate-fade-left animate-once animate-duration-[3000ms]'>
-          <h1 className={`${styles.heroHeadText}`}>
-            Hola, soy
-            <span className='bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-800 ml-4'>
-              Francisco
-            </span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 inline-block`}>
-            Desarrollo aplicaciones web FullStack
-          </p>
-        </div>
+        {/* ... (código anterior) */}
 
         <div className='flex justify-center mx-auto relative'>
           <div className='w-full max-w-lg'>
@@ -39,30 +28,31 @@ const Hero = () => {
               alt='Francisco'
             />
 
-          <div          ontouchstar={() => scrollToSection("sobre-mi")}
-          style={{
-            cursor: "pointer",
-            backgroundColor: "transparent",
-            border: "none",
-            padding: 0,
-          }} className="z-90 animate-bounce absolute bottom-[-30px] left-[45%] transform -translate-x-1/2 xs:bottom-[-50px] bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
-            <svg
-              className="w-6 h-6 text-violet-500 flex justify-center"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+            {/* Cambiamos el div con onClick a un botón */}
+            <button
+              onClick={() => scrollToSection("sobre-mi")}
+              style={{
+                cursor: "pointer",
+                border: "none",
+                padding: 0,
+              }}
+              className="z-90 animate-bounce absolute bottom-[-30px] left-[45%] transform -translate-x-1/2 xs:bottom-[-50px] bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center"
             >
-              <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-            </svg>
-          </div>
+              <svg
+                className="w-6 h-6 text-violet-500 flex justify-center"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
-
-
     </>
   );
 };
